e6c8e852b60e707810d1198476e96657e7b6b694
MBODM <webmaster@mbodm.com>
2022-05-20 09:46:55 +0200

    Developing (see diff).

--- src/console.rs
+++ src/console.rs
@@ -1,4 +1,4 @@
-use crate::errors;
+use crate::{errors, core::PackageInfo};
 use std::io::{stdout, Write};
 
 pub fn print(s: &str) {
@@ -10,3 +10,154 @@ pub fn print_line(s: &str) {
     println!("{s}");
     stdout().flush().expect(errors::UNRECOVERABLE);
 }
+
+pub fn print_usage(exe_file: &str, show_error: bool) {
+    if show_error {
+        println!("Error: Unknown parameter(s).");
+        println!();
+    }
+
+    println!("Usage: {exe_file} [--no-log] [--no-confirm]");
+    println!("");
+    println!("  --no-log      Don´t create log file (useful when running from a folder without write permissions)");
+    println!("  --no-confirm  Don´t ask for update confirmation (useful for script integration)");
+    println!("");
+    println!(
+        "For more information have a look at the GitHub page (https://github.com/MBODM/wingetupd"
+    );
+}
+
+//         public static void ShowPackageFileEntries(IEnumerable<string> packageFileEntries)
+//         {
+//             println!($"Found package-file, containing {packageFileEntries.Count()} {EntryOrEntries(packageFileEntries)}.");
+//         }
+
+pub fn show_invalid_packages_error(packages: Vec<String>) {
+    println!("Error: The package-file contains invalid entries.");
+    println!();
+    println!("The following package-file entries are not valid WinGet package id´s:");
+    //ListPackages(invalidPackages);
+    println!();
+    println!("You can use 'winget search' to list all valid package id´s.");
+    println!();
+    println!("Please verify package-file and try again.");
+}
+
+pub fn show_non_installed_packages_error() {
+    println!("Error: The package-file contains non-installed packages.");
+    println!();
+    println!("The following package-file entries are valid WinGet package id´s,");
+    println!("but those packages are not already installed on this machine yet:");
+    // ListPackages(nonInstalledPackages);
+    println!();
+    println!("You can use 'winget list' to show all installed packages and their package id´s.");
+    println!();
+    println!("Please verify package-file and try again.");
+}
+
+pub fn show_summary(package_infos: Vec<PackageInfo>)
+{
+    let valid_packages: Vec<String> = package_infos.iter().filter(|x| x.is_valid).map(|y|&y.package).collect();
+
+//             var validPackages = packageInfos.Where(packageInfo => packageInfo.IsValid).Select(packageInfo => packageInfo.Package);
+//             var installedPackages = packageInfos.Where(packageInfo => packageInfo.IsInstalled).Select(packageInfo => packageInfo.Package);
+//             var updatablePackages = packageInfos.Where(packageInfo => packageInfo.IsUpdatable);
+
+//             println!($"{packageInfos.Count()} package-file {EntryOrEntries(packageInfos)} processed.");
+
+//             println!($"{validPackages.Count()} package-file {EntryOrEntries(packageInfos)} validated.");
+
+//             println!($"{installedPackages.Count()} {PackageOrPackages(installedPackages)} installed:");
+//             ListPackages(installedPackages);
+
+//             Console.Write($"{updatablePackages.Count()} {PackageOrPackages(updatablePackages)} updatable");
+//             if (updatablePackages.Any())
+//             {
+//                 println!(":");
+//                 ListUpdateablePackages(updatablePackages);
+//             }
+//             else
+//             {
+//                 println!(".");
+//             }
+
+}
+
+
+//         public static bool AskUpdateQuestion(IEnumerable<string> updateablePackages)
+//         {
+//             Console.Write($"Update {updateablePackages.Count()} {PackageOrPackages(updateablePackages)} ? [y/N]: ");
+
+//             while (true)
+//             {
+//                 var key = Console.ReadKey(true).Key;
+
+//                 if (key == ConsoleKey.N || key == ConsoleKey.Enter)
+//                 {
+//                     Console.Write("N");
+//                     println!();
+//                     println!();
+
+//                     return false;
+//                 }
+
+//                 if (key == ConsoleKey.Y)
+//                 {
+//                     Console.Write("y");
+//                     println!();
+//                     println!();
+
+//                     return true;
+//                 }
+//             }
+//         }
+
+//         public static void ShowUpdatedPackages(IEnumerable<string> updatedPackages)
+//         {
+//             println!();
+//             println!($"{updatedPackages.Count()} {PackageOrPackages(updatedPackages)} updated:");
+
+//             ListPackages(updatedPackages);
+//         }
+
+//         public static void ShowGoodByeMessage()
+//         {
+//             println!("Have a nice day.");
+//         }
+
+//         public static void ShowWinGetError(string error, string log)
+//         {
+//             var winGetLogFolder = Path.Combine(
+//                 Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData),
+//                 @"Packages\Microsoft.DesktopAppInstaller_8wekyb3d8bbwe\LocalState\DiagOutputDir");
+
+//             var winGetWebSite = "https://docs.microsoft.com/en-us/windows/package-manager/winget";
+
+//             println!();
+//             println!();
+//             println!($"Error: {error}");
+//             println!();
+//             println!($"For details have a look at the log file ('{log}').");
+//             println!();
+//             println!("For even more details have a look at WinGet´s own log files:");
+//             println!(winGetLogFolder);
+//             println!();
+//             println!("You can also find further information on the WinGet site: ");
+//             println!(winGetWebSite);
+//         }
+
+//         private static string EntryOrEntries<T>(IEnumerable<T> enumerable) =>
+//             SingularOrPlural(enumerable, "entry", "entries");
+
+//         private static string PackageOrPackages<T>(IEnumerable<T> enumerable) =>
+//             SingularOrPlural(enumerable, "package", "packages");
+
+//         private static string SingularOrPlural<T>(IEnumerable<T> enumerable, string singular, string plural) =>
+//             enumerable.Count() == 1 ? singular : plural;
+
+//         private static void ListPackages(IEnumerable<string> packages) =>
+//             packages.ToList().ForEach(package => println!($"  - {package}"));
+
+//         private static void ListUpdateablePackages(IEnumerable<PackageInfo> packageInfos) =>
+//             packageInfos.ToList().ForEach(pi => println!($"  - {pi.Package} {pi.InstalledVersion} => {pi.UpdateVersion}"));
+//     }
